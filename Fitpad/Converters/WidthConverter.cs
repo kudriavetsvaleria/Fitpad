42060c1f786f231fda1317c667816bab5ae79cd7
Kudriavets Valeria <vvkudriavetsvaleria@gmail.com>
2025-02-17 19:37:07 +0200

    Общие улучшение внешнего вида страниц

--- Fitpad/Converters/WidthConverter.cs
+++ Fitpad/Converters/WidthConverter.cs
@@ -9,7 +9,7 @@ namespace Fitpad.Converters
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             bool isExpanded = (bool)value;
-            return isExpanded ? 220 : 78; // 80 вместо 50 для расширенной узкой панели
+            return isExpanded ? 220 : 72; // 80 вместо 50 для расширенной узкой панели
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
